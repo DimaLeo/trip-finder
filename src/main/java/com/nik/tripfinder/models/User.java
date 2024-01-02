73a8e82e914a546a415248e87bcfe6a191230d76
Nick Pantelidis <thunderarea.np@gmail.com>
2024-01-02 16:42:44 +0200

    Unique user name

--- src/main/java/com/nik/tripfinder/models/User.java
+++ src/main/java/com/nik/tripfinder/models/User.java
@@ -8,6 +8,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "username", unique = true)
     private String username;
     private String password;
     @Column(name = "user_type")
