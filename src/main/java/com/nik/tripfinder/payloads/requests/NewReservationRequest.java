b91c0f4456a29012402b6f969de1b7fa6d560b14
Nick Pantelidis <thunderarea.np@gmail.com>
2024-01-10 21:45:31 +0200

    Change in the name of the field in new reservation request

--- src/main/java/com/nik/tripfinder/payloads/requests/NewReservationRequest.java
+++ src/main/java/com/nik/tripfinder/payloads/requests/NewReservationRequest.java
@@ -1,11 +1,8 @@
 package com.nik.tripfinder.payloads.requests;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class NewReservationRequest {
 
     Integer userId;
-    @JsonProperty("trip_id")
     Long tripId;
 
     public Integer getUserId() {
